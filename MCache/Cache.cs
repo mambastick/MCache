fa20dc2b490fbbe840c894604fb108c76eb4e09a
Neelov Eugene <neelov.ev@mail.ru>
2024-01-01 15:10:00 +0300

    Initial commit

---
+++ MCache/Cache.cs
@@ -0,0 +1,25 @@
+﻿namespace MCache;
+
+public class MCache<T>
+{
+    private readonly Dictionary<object, T?> _cache = new();
+
+    public void AddOrUpdate(object key, T data) => _cache[key] = data;
+
+    public T? Get(object key)
+    {
+        if (!_cache.TryGetValue(key, out var cacheItem))
+            return default; 
+        if (cacheItem != null)
+        {
+            return cacheItem;
+        }
+        
+        _cache.Remove(key);
+        return default;
+    }
+
+    public void Remove(object key) => _cache.Remove(key);
+    
+    public void Clear() => _cache.Clear();
+}
